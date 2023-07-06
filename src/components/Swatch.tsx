87708c09f936b07b349c565b7957a99bae9954d7
Amanda <hello@amandahinton.com>
2023-07-05 20:11:52 -0400

    update swatch size and label

--- src/components/Swatch.tsx
+++ src/components/Swatch.tsx
@@ -1,12 +1,14 @@
 import { useColor } from "../contexts/ColorContext";
-import { contrastText } from "../utils/helpers";
+import { contrastText, cssColorValue } from "../utils/helpers";
 import { ColorMode } from "../types/enums";
 
 export function Swatch() {
   const { color } = useColor();
   const { colorMode } = color;
+  const colorCode = color[colorMode];
 
   const labelColor = contrastText(color.HEX);
+  const cssValue = cssColorValue(colorMode, colorCode);
 
   let swatchColor;
 
@@ -20,12 +22,26 @@ export function Swatch() {
     case ColorMode.RGBA:
       swatchColor = `rgba(${color.RGBA.r}, ${color.RGBA.r}, ${color.RGBA.r}, ${color.RGBA.a})`;
       break;
-      break;
   }
 
   return (
-    <div style={{ backgroundColor: swatchColor }}>
-      <p style={{ color: labelColor }}>{`${colorMode}: ${color[colorMode]}`}</p>
+    <div
+      style={{
+        backgroundColor: cssValue,
+        height: 100,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 20,
+      }}
+    >
+      <p
+        style={{
+          color: labelColor,
+        }}
+      >
+        {cssValue}
+      </p>
     </div>
   );
 }
