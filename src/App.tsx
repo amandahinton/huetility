144ba366307328df3dddc2c9993364eb45e97afb
Amanda <hello@amandahinton.com>
2023-07-06 17:17:38 -0400

    add tints and shades to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { CSSValues, ColorPicker, Swatch } from "./components";
+import { CSSValues, ColorPicker, Shades, Swatch, Tints } from "./components";
 import { ColorProvider } from "./contexts/ColorContext";
 import "./App.css";
 
 function App() {
   return (
     <ColorProvider>
-      <h1>Color</h1>
       <ColorPicker />
       <Swatch />
       <CSSValues />
+      <Tints />
+      <Shades />
     </ColorProvider>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ export * from "./ColorPickerHEX.tsx";
 export * from "./ColorPickerRGB.tsx";
 export * from "./ColorPickerRGBA.tsx";
 export * from "./CSSValues.tsx";
+export * from "./Shades.tsx";
 export * from "./Swatch.tsx";
+export * from "./Tints.tsx";
