3003d98a21c1c9fe5f80cfdb184ae13bacf8cad0
Amanda <hello@amandahinton.com>
2023-07-06 13:37:02 -0400

    remove form so return doesn't submit

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -17,24 +17,23 @@ export function ColorPicker() {
   return (
     <>
       <h3>Select Color</h3>
-      <form>
-        <div>
-          <label htmlFor="colorMode">Color mode</label>
-          <select
-            required
-            value={color.colorMode}
-            onChange={(e) =>
-              setMode(ColorMode[e.target.value as keyof typeof ColorMode])
-            }
-          >
-            {Object.keys(ColorMode).map((mode) => (
-              <option key={mode} value={mode}>
-                {mode}
-              </option>
-            ))}
-          </select>
-        </div>
-      </form>
+      <div>
+        <label htmlFor="colorMode">Color mode</label>
+        <select
+          required
+          value={color.colorMode}
+          onChange={(e) =>
+            setMode(ColorMode[e.target.value as keyof typeof ColorMode])
+          }
+        >
+          {Object.keys(ColorMode).map((mode) => (
+            <option key={mode} value={mode}>
+              {mode}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <PickerInput />
     </>
   );
