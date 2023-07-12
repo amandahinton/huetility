ac5654a443e70e01ca5b8d659f57662a6ae03e14
Amanda <hello@amandahinton.com>
2023-07-12 15:43:34 -0400

    add CSS rule strings for b&w to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,10 @@
 export const BLACK_HEXCODE = "#000000";
 export const BLACK_RGB = { r: 0, g: 0, b: 0 };
 export const BLACK_RGBA = { r: 0, g: 0, b: 0, a: 1 };
+export const BLACK_RGB_CSS = "rgb(0, 0, 0)";
 
 export const WHITE_HEXCODE = "#ffffff";
 export const WHITE_RGB = { r: 255, g: 255, b: 255 };
 export const WHITE_RGBA = { r: 255, g: 255, b: 255, a: 1 };
+export const WHITE_RGB_CSS = "rgb(255, 255, 255)";
+
