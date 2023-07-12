0c25a833703583b4761aada60e6df67878096a51
Amanda <hello@amandahinton.com>
2023-07-11 21:33:25 -0400

    write helpers to check if rgb or rgba

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { hexcodeToRGB } from "./translations.ts/fromHEX";
 import { BLACK_HEXCODE, WHITE_HEXCODE } from "./constants";
+import { RGB, RGBA } from "../types/types";
 import { ColorMode } from "../types/enums";
 
 export const isHexcode = (hexcode: string): boolean => {
@@ -7,6 +8,30 @@ export const isHexcode = (hexcode: string): boolean => {
     /^#([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
   );
   return pattern.test(hexcode);
+};
+
+export const isRGB = (rgb: RGB): boolean => {
+  return (
+    rgb.r >= 0 &&
+    rgb.r <= 255 &&
+    rgb.g >= 0 &&
+    rgb.g <= 255 &&
+    rgb.b >= 0 &&
+    rgb.b <= 255
+  );
+};
+
+export const isRGBA = (rgba: RGBA): boolean => {
+  return (
+    rgba.r >= 0 &&
+    rgba.r <= 255 &&
+    rgba.g >= 0 &&
+    rgba.g <= 255 &&
+    rgba.b >= 0 &&
+    rgba.b <= 255 &&
+    rgba.a >= 0 &&
+    rgba.a <= 1
+  );
 };
 
 export const contrastText = (hexcode: string): string => {
