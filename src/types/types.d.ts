06f8d3a1775fe229daae2bed1e03335352686b49
Amanda <hello@amandahinton.com>
2023-07-13 11:39:22 -0400

    add colorspace info

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,12 +1,20 @@
 import { ColorMode } from "./enums";
 
+// sRGB color space: hsl(), hwb(), rgb();
+// CIELAB color space: lab(), lch();
+// Oklab color space: oklab(), oklch();
+// also RGB but not in css: hsb/hsv
+
 export type ColorData = {
   colorMode: ColorMode;
   HEX: string;
   HSL?: HSL;
+  HSLA?: HSLA;
   HWB? = HWB;
   LAB? = LAB;
+  OKLAB? = LAB;
   LCH? = LCH;
+  OKLCH? = LCH;
   RGB: RGB;
   RGBA: RGBA;
 };
@@ -17,36 +25,47 @@ export type ColorData = {
 // saturation: 0-100 (gray to full color)
 // lightness (black or white added to hue): 0-100 (black to white)
 export type HSL = {
-  h: string;
-  s: string;
-  l: string;
+  h: number;
+  s: number;
+  l: number;
+};
+
+// hue: 0-360
+// saturation: 0-100 (gray to full color)
+// lightness (black or white added to hue): 0-100 (black to white)
+// alpha/transparency: 0-1 decimal
+export type HSL = {
+  h: number;
+  s: number;
+  l: number;
+  a: number;
 };
 
 // hue: 0-360
 // whiteness: 0-100 (full color to white)
 // blackness: 0-100 (full color to black)
 export type HWB = {
-  h: string;
-  w: string;
-  b: string;
+  h: number;
+  w: number;
+  b: number;
 };
 
 // lightness
 // red/green
 // blue/yellow
 export type LAB = {
-  l: string;
-  a: string;
-  b: string;
+  l: number;
+  a: number;
+  b: number;
 };
 
 // lightness (brightness vs illuminated white)
 // chroma: 0-131
 // hue
 export type LCH = {
-  l: string;
-  c: string;
-  h: string;
+  l: number;
+  c: number;
+  h: number;
 };
 
 // red: 0-255 (none to full color)
