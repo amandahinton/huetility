c5434b7b88de2d0b9c852dc35f13a7fd82a8d90a
Amanda <hello@amandahinton.com>
2023-07-13 14:27:47 -0400

    add pixel clicker to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { CSSValues, ColorPicker, ScaleShades, Swatch, ScaleTints } from "./components";
+import {
+  ColorPicker,
+  ScaleShades,
+  ColorSpace,
+  CSSValues,
+  ScaleTints,
+} from "./components";
 import { ColorProvider } from "./contexts/ColorContext";
 import "./App.css";
-import "../src/components/index.css"
+import "../src/components/index.css";
 
 function App() {
   return (
     <ColorProvider>
       <h1>HUETILITY</h1>
+      <ColorSpace />
       <ColorPicker />
       <CSSValues />
       <ScaleTints />
