dd8ea16f3781dd73dfa679577b67927de166acdb
Amanda <hello@amandahinton.com>
2023-07-14 12:46:33 -0400

    add will read frequently flag to canvas context

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -26,7 +26,7 @@ export function Canvas({
     const canvas = canvasRef.current;
     if (canvas == null) return;
 
-    const canvasContext: CanvasRenderingContext2D = canvas.getContext("2d");
+    const canvasContext: CanvasRenderingContext2D = canvas.getContext("2d", { willReadFrequently: true });
     if (canvasContext == null) return;
 
     if (onClick) {
