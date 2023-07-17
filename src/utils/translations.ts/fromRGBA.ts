017009254df5f579180783db6979ef80c920fb08
Amanda <hello@amandahinton.com>
2023-07-17 16:05:10 -0400

    fix bug related to rgba translation

--- src/utils/translations.ts/fromRGBA.ts
+++ src/utils/translations.ts/fromRGBA.ts
@@ -3,19 +3,33 @@ import { BLACK_HEXCODE } from "../constants";
 import { RGB, RGBA } from "../../types/types";
 
 export const RGBAToHexcode = (rgba: RGBA): string => {
-  let aValue = Math.round(rgba.a * 255);
+  const aValue = Math.round(rgba.a * 255);
 
-  let red = rgba.r < 9 ? "0" + rgba.r.toString() : rgba.r.toString(16);
-  let green = rgba.g < 9 ? "0" + rgba.g.toString() : rgba.g.toString(16);
-  let blue = rgba.b < 9 ? "0" + rgba.b.toString() : rgba.b.toString(16);
-  let alpha = aValue < 9 ? "0" + aValue.toString() : aValue.toString(16);
+  const twoCharHex = (hexFragment: string): string => {
+    if (hexFragment.length === 1) {
+      return "0" + hexFragment;
+    } else if (hexFragment.length === 0) {
+      return "00";
+    } else {
+      return hexFragment.slice(0, 2);
+    }
+  };
+
+  const red = twoCharHex(rgba.r.toString(16));
+  const green = twoCharHex(rgba.g.toString(16));
+  const blue = twoCharHex(rgba.b.toString(16));
+  const alpha = twoCharHex(aValue.toString(16));
 
   let hexcode = "#" + red + green + blue + alpha;
 
   if (isHexcode(hexcode)) {
     return hexcode;
   } else {
-    console.error("Error: improper rgba input, defaulting to black hexcode");
+    console.error(
+      `Error: improper rgba ${JSON.stringify(
+        rgba
+      )}, defaulting to black hexcode`
+    );
     return BLACK_HEXCODE;
   }
 };
