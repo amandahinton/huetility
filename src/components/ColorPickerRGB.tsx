4d1b12ed1da90468661b6526a5e48393a3073ae1
Amanda <hello@amandahinton.com>
2023-07-17 21:02:16 -0400

    fix flex stacking of inputs and errors

--- src/components/ColorPickerRGB.tsx
+++ src/components/ColorPickerRGB.tsx
@@ -26,90 +26,96 @@ export function ColorPickerRGB() {
   return (
     <div className="huetility-code-input-container">
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgb-picker-r">R:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgb-picker-r">R:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgb-picker-r"
+              name="rgbPickerR"
+              min={0}
+              max={255}
+              value={RGBInput.r}
+              onChange={(e) => handleRGBChange("r", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgb-picker-r"
-            name="rgbPickerR"
-            min={0}
-            max={255}
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            id="rgb-slider-r"
+            name="rgbSliderR"
             value={RGBInput.r}
             onChange={(e) => handleRGBChange("r", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="255"
-          step="1"
-          id="rgb-slider-r"
-          name="rgbSliderR"
-          value={RGBInput.r}
-          onChange={(e) => handleRGBChange("r", e)}
-        />
         {(RGBInput.r < 0 || RGBInput.r > 255) && (
           <small className="huetility-input-error">Enter a number 0-255</small>
         )}
       </div>
 
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgb-picker-g">G:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgb-picker-g">G:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgb-picker-g"
+              name="rgbPickerG"
+              min={0}
+              max={255}
+              value={RGBInput.g}
+              onChange={(e) => handleRGBChange("g", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgb-picker-g"
-            name="rgbPickerG"
-            min={0}
-            max={255}
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            id="rgb-slider-g"
+            name="rgbSliderG"
             value={RGBInput.g}
             onChange={(e) => handleRGBChange("g", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="255"
-          step="1"
-          id="rgb-slider-g"
-          name="rgbSliderG"
-          value={RGBInput.g}
-          onChange={(e) => handleRGBChange("g", e)}
-        />
         {(RGBInput.g < 0 || RGBInput.g > 255) && (
           <small className="huetility-input-error">Enter a number 0-255</small>
         )}
       </div>
 
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgb-picker-b">B:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgb-picker-b">B:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgb-picker-b"
+              name="rgbPickerB"
+              min={0}
+              max={255}
+              value={RGBInput.b}
+              onChange={(e) => handleRGBChange("b", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgb-picker-b"
-            name="rgbPickerB"
-            min={0}
-            max={255}
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            id="rgb-slider-b"
+            name="rgbSliderB"
             value={RGBInput.b}
             onChange={(e) => handleRGBChange("b", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="255"
-          step="1"
-          id="rgb-slider-b"
-          name="rgbSliderB"
-          value={RGBInput.b}
-          onChange={(e) => handleRGBChange("b", e)}
-        />
         {(RGBInput.b < 0 || RGBInput.b > 255) && (
           <small className="huetility-input-error">Enter a number 0-255</small>
         )}
--- src/components/ColorPickerRGBA.tsx
+++ src/components/ColorPickerRGBA.tsx
@@ -26,121 +26,129 @@ export function ColorPickerRGBA() {
   return (
     <div className="huetility-code-input-container">
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgba-picker-r">R:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgba-picker-r">R:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgba-picker-r"
+              name="rgbaPickerR"
+              min={0}
+              max={255}
+              value={RGBAInput.r}
+              onChange={(e) => handleRGBAChange("r", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgba-picker-r"
-            name="rgbaPickerR"
-            min={0}
-            max={255}
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            id="rgba-slider-r"
+            name="rgbaSliderR"
             value={RGBAInput.r}
             onChange={(e) => handleRGBAChange("r", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="255"
-          step="1"
-          id="rgba-slider-r"
-          name="rgbaSliderR"
-          value={RGBAInput.r}
-          onChange={(e) => handleRGBAChange("r", e)}
-        />
         {(RGBAInput.r < 0 || RGBAInput.r > 255) && (
           <small className="huetility-input-error">Enter a number 0-255</small>
         )}
       </div>
 
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgba-picker-g">G:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgba-picker-g">G:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgba-picker-g"
+              name="rgbaPickerG"
+              min={0}
+              max={255}
+              value={RGBAInput.g}
+              onChange={(e) => handleRGBAChange("g", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgba-picker-g"
-            name="rgbaPickerG"
-            min={0}
-            max={255}
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            id="rgba-slider-g"
+            name="rgbaSliderG"
             value={RGBAInput.g}
             onChange={(e) => handleRGBAChange("g", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="255"
-          step="1"
-          id="rgba-slider-g"
-          name="rgbaSliderG"
-          value={RGBAInput.g}
-          onChange={(e) => handleRGBAChange("g", e)}
-        />
         {(RGBAInput.g < 0 || RGBAInput.g > 255) && (
           <small className="huetility-input-error">Enter a number 0-255</small>
         )}
       </div>
 
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgba-picker-b">B:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgba-picker-b">B:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgba-picker-b"
+              name="rgbaPickerB"
+              min={0}
+              max={255}
+              value={RGBAInput.b}
+              onChange={(e) => handleRGBAChange("b", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgba-picker-b"
-            name="rgbaPickerB"
-            min={0}
-            max={255}
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            id="rgba-slider-b"
+            name="rgbaSliderB"
             value={RGBAInput.b}
             onChange={(e) => handleRGBAChange("b", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="255"
-          step="1"
-          id="rgba-slider-b"
-          name="rgbaSliderB"
-          value={RGBAInput.b}
-          onChange={(e) => handleRGBAChange("b", e)}
-        />
         {(RGBAInput.b < 0 || RGBAInput.b > 255) && (
           <small className="huetility-input-error">Enter a number 0-255</small>
         )}
       </div>
 
       <div className="huetility-code-input">
-        <div className="huetility-code-number-input">
-          <label htmlFor="rgba-picker-a">A:</label>
+        <div className="huetility-code-input-group">
+          <div className="huetility-code-number-input">
+            <label htmlFor="rgba-picker-a">A:</label>
+            <input
+              required
+              className="huetility-number-input"
+              type="number"
+              id="rgba-picker-a"
+              name="rgbaPickerA"
+              min="0"
+              max="1"
+              step="0.1"
+              value={RGBAInput.a}
+              onChange={(e) => handleRGBAChange("a", e)}
+            />
+          </div>
           <input
-            required
-            className="huetility-number-input"
-            type="number"
-            id="rgba-picker-a"
-            name="rgbaPickerA"
+            type="range"
             min="0"
             max="1"
             step="0.1"
+            id="rgba-slider-a"
+            name="rgbaSliderA"
             value={RGBAInput.a}
             onChange={(e) => handleRGBAChange("a", e)}
           />
         </div>
-        <input
-          type="range"
-          min="0"
-          max="1"
-          step="0.1"
-          id="rgba-slider-a"
-          name="rgbaSliderA"
-          value={RGBAInput.a}
-          onChange={(e) => handleRGBAChange("a", e)}
-        />
         {(RGBAInput.a < 0 || RGBAInput.a > 1) && (
           <small className="huetility-input-error">
             Enter a decimal number 0-1
