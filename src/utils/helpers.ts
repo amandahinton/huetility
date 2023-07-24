15e3fa26ceedf014fb27e181323d86f0e308b4a0
Amanda <hello@amandahinton.com>
2023-07-24 17:51:41 -0400

    wip - start luminance helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { hexcodeToRGBA } from "./translations.ts/fromHEX";
+import { hexcodeToRGBA, hexcodeToRGB } from "./translations.ts/fromHEX";
 import { BLACK_HEXCODE, WHITE_HEXCODE } from "./constants";
 import { RGB, RGBA } from "../types/types";
 import { ColorMode } from "../types/enums";
@@ -70,17 +70,83 @@ export const isWhite = (mode: ColorMode, code: any): boolean => {
 
   return isWhite;
 };
+/*
+https://www.w3.org/TR/WCAG20/
+relative brightness of point in colorspace
+normalized to 0 for darkest black and 1 for lightest white
+contrast:
+difference in perceived luminance/brightness between colors
+ratio from 1:1 white:white to 21:1 black:white
+WCAG standard:
+text and interactive elements at least 4.5:1 (20/40 vision)
+large-scale text at least 3:1
+*/
+
+export const approximateRGBFromRGBA = (
+  foregroundColor: RGBA,
+  backgroundColor: RGB
+): RGB => {
+  // source - normalize foreground RGBA channels
+  const foreR = foregroundColor.r / 255;
+  const foreG = foregroundColor.g / 255;
+  const foreB = foregroundColor.b / 255;
+  const foreA = foregroundColor.a;
+
+  // matte - normalize background RGB channels
+  const backR = backgroundColor.r / 255;
+  const backG = backgroundColor.g / 255;
+  const backB = backgroundColor.b / 255;
+
+  // convert from transparent color on background to flat RGB
+  let transformedR = (1 - foreA) * backR + foreA * foreR;
+  let transformedG = (1 - foreA) * backG + foreA * foreG;
+  let transformedB = (1 - foreA) * backB + foreA * foreB;
+  // cap at 255 if calculation was overexposed
+  if (transformedR > 1) transformedR = 255;
+  if (transformedG > 1) transformedG = 255;
+  if (transformedB > 1) transformedB = 255;
+
+  return { r: transformedR, g: transformedG, b: transformedB };
+};
+
+const channelLuminance = (channelValue: number): number => {
+  let output = channelValue / 255;
+  return output <= 0.03928
+    ? output / 12.92
+    : Math.pow((output + 0.055) / 1.055, 2.4);
+};
+
+export const relativeLuminance = (hexcode: string): number | undefined => {
+  if (!isHexcode(hexcode)) return undefined;
+
+  try {
+    const rgb = hexcodeToRGB(hexcode);
+    if (rgb) {
+      const R = channelLuminance(rgb.r);
+      const G = channelLuminance(rgb.g);
+      const B = channelLuminance(rgb.b);
+      // For sRGB colorspace, relative luminance defined as...
+      return 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    }
+  } catch (error) {
+    console.error("Could not calculate luminance:", error);
+  }
+
+  return undefined;
+};
 
 export const contrastText = (hexcode: string): string => {
   if (!isHexcode(hexcode)) return BLACK_HEXCODE;
 
   try {
-    const rgba = hexcodeToRGBA(hexcode);
-    if (rgba) {
-      if (rgba.a < 0.3) return BLACK_HEXCODE;
+    if (hexcode.length == 9) {
+      const alpha = hexcode.slice(8);
+      // if transparent on light background, use black
+      if (parseInt(alpha, 16) <= 0.3) return BLACK_HEXCODE;
 
-      const luminance = rgba.r * 0.299 + rgba.g * 0.587 + rgba.b * 0.114;
-      return luminance > 150 ? BLACK_HEXCODE : WHITE_HEXCODE;
+      // choose black or white text
+      const luminance = relativeLuminance(hexcode);
+      if (luminance) return luminance > 150 ? BLACK_HEXCODE : WHITE_HEXCODE;
     }
   } catch (error) {
     console.error("Could not calculate text color:", error);
