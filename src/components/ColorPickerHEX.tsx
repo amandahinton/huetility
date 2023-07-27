255f5beb623a2768a09207cf2c3ef60779138b5f
Amanda <hello@amandahinton.com>
2023-07-27 18:16:48 -0400

    make hex input reusable

--- src/components/ColorPickerHEX.tsx
+++ src/components/ColorPickerHEX.tsx
@@ -1,49 +1,8 @@
-import React from "react";
 import { useColor } from "../contexts/ColorContext";
-import { isHexcode, isPartialHexcode } from "../utils/helpers";
+import { InputHex } from "../components";
 
 export function ColorPickerHEX() {
   const { color, setHEX } = useColor();
-  const { HEX } = color;
 
-  const [hexInput, setHexInput] = React.useState<string>(HEX);
-
-  React.useEffect(() => {
-    setHexInput(HEX);
-  }, [HEX]);
-
-  const handleHexChange = React.useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-
-      let input = e.target.value;
-      if (input[0] !== "#") input = "#" + input;
-      if (isPartialHexcode(input)) setHexInput(input);
-
-      if (isHexcode(input)) setHEX(input);
-    },
-    [setHEX]
-  );
-
-  return (
-    <div className="huetility-code-input-container">
-      <div className="huetility-code-input">
-        <div className="huetility-code-string-input">
-          <label htmlFor="hexPicker">Hexcode:</label>
-          <input
-            required
-            className="huetility-string-input"
-            type="text"
-            id="hexPicker"
-            name="hexPicker"
-            value={hexInput}
-            onChange={handleHexChange}
-          />
-        </div>
-        {!isHexcode(hexInput) && (
-          <small className="huetility-input-error">Enter a valid hexcode</small>
-        )}
-      </div>
-    </div>
-  );
+  return <InputHex onChange={(input: string) => setHEX(input)} color={color} />;
 }
---
+++ src/components/InputHex.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { isHexcode, isPartialHexcode } from "../utils/helpers";
+import { ColorCodes } from "../types/types";
+
+type Props = {
+  onChange: (hexcode: string) => void;
+  color: ColorCodes;
+};
+
+export function InputHex({ onChange, color }: Props) {
+  const { HEX } = color;
+
+  const [hexInput, setHexInput] = React.useState<string>(HEX);
+
+  React.useEffect(() => {
+    setHexInput(HEX);
+  }, [HEX]);
+
+  const handleHexChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+
+      let input = e.target.value;
+      if (input[0] !== "#") input = "#" + input;
+      if (isPartialHexcode(input)) setHexInput(input);
+
+      if (isHexcode(input)) onChange(input);
+    },
+    [onChange]
+  );
+
+  return (
+    <div className="huetility-code-input-container">
+      <div className="huetility-code-input">
+        <div className="huetility-code-string-input">
+          <label htmlFor="hexPicker">Hexcode:</label>
+          <input
+            required
+            className="huetility-string-input"
+            type="text"
+            id="hexPicker"
+            name="hexPicker"
+            value={hexInput}
+            onChange={handleHexChange}
+          />
+        </div>
+        {!isHexcode(hexInput) && (
+          <small className="huetility-input-error">Enter a valid hexcode</small>
+        )}
+      </div>
+    </div>
+  );
+}
