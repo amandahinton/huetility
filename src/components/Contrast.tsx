9d1dd93a5fcfcf7e8f4df3ffeeb1704550cc1218
Amanda <hello@amandahinton.com>
2023-07-28 19:42:25 -0400

    add transparency note on contrast

--- src/components/Contrast.tsx
+++ src/components/Contrast.tsx
@@ -1,11 +1,20 @@
+import { useColor } from "../contexts/ColorContext";
 import { ContrastChecker } from "../components";
 import { BLACK_CODES, WHITE_CODES } from "../utils/constants";
-import "./Contrast.css"
+import "./Contrast.css";
 
 export function Contrast() {
+  const { color } = useColor();
+
   return (
     <div className="huetility-component-container">
       <h2 className="huetility-component-title">Color Contrast</h2>
+      {color.RGBA.a < 1 && (
+        <p>
+          Selected color {color.HEX} has transparency, which affects
+          contrast
+        </p>
+      )}
       <div className="huetility-contrast-input-container">
         <ContrastChecker contrastColor={WHITE_CODES} />
         <ContrastChecker contrastColor={BLACK_CODES} />
