74ea1816f0dd8eb91253e9343739793eea19d2d8
Amanda <hello@amandahinton.com>
2023-07-28 22:10:58 -0400

    remove transparency message, it works

--- src/components/Contrast.tsx
+++ src/components/Contrast.tsx
@@ -1,20 +1,11 @@
-import { useColor } from "../contexts/ColorContext";
 import { ContrastChecker } from "../components";
 import { BLACK_CODES, WHITE_CODES } from "../utils/constants";
 import "./Contrast.css";
 
 export function Contrast() {
-  const { color } = useColor();
-
   return (
     <div className="huetility-component-container">
       <h2 className="huetility-component-title">Color Contrast</h2>
-      {color.RGBA.a < 1 && (
-        <p>
-          Selected color {color.HEX} has transparency, which affects
-          contrast
-        </p>
-      )}
       <div className="huetility-contrast-input-container">
         <ContrastChecker contrastColor={WHITE_CODES} />
         <ContrastChecker contrastColor={BLACK_CODES} />
