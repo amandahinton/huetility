414eba6238116f917c36f5634b6491b33e2acfba
Amanda <hello@amandahinton.com>
2023-08-02 17:03:14 -0400

    interpolate style names for CDV component

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -1,44 +1,42 @@
 import { useColor } from "../contexts/ColorContext";
 import { contrast, perceivedColors } from "../utils/helpers";
+import { BLACK_CODES, WHITE_CODES } from "../utils/constants";
 import { PerceivedColor } from "../types/types";
 import "./Vision.css";
-import { BLACK_CODES, WHITE_CODES } from "../utils/constants";
 
 export function Vision() {
   const { color } = useColor();
 
-  const perceived: PerceivedColor[] = perceivedColors(color);
+  const visionDeficiencyColors: PerceivedColor[] = perceivedColors(color);
 
   return (
     <div className="huetility-component-container huetility-outer">
-      <h2 className="huetility-component-title">Perceived Colors</h2>
+      <h2 className="huetility-component-title">Vision Deficiency</h2>
       <div className="huetility-vision-container">
-        {perceived.map((viz) => (
+        {visionDeficiencyColors.map((viz) => (
           <div key={viz.name} className="huetility-vision-display">
             <div
-              className="huetility-vision-swatch huetility-bordered"
+              className={`huetility-vision-swatch huetility-bordered huetility-${viz.name}-vision-swatch`}
               style={{ backgroundColor: viz.color.HEX }}
             >
               <div className="huetility-vision-text-samples">
                 <p
                   style={{ color: "#ffffff" }}
-                  className={
-                    viz.name === "Diminished" ? "huetility-blurred-text" : ""
-                  }
+                  className={`huetility-${viz.name}-vision-text`}
                 >
                   White {contrast(viz.color, WHITE_CODES)}
                 </p>
                 <p
                   style={{ color: "#000000" }}
-                  className={
-                    viz.name === "Diminished" ? "huetility-blurred-text" : ""
-                  }
+                  className={`huetility-${viz.name}-vision-text`}
                 >
                   Black {contrast(viz.color, BLACK_CODES)}
                 </p>
               </div>
             </div>
-            <p className="huetility-vision-category">{viz.name}</p>
+            <p className="huetility-vision-category">
+              {viz.name.charAt(0).toUpperCase() + viz.name.slice(1)}
+            </p>
             <small>{viz.description}</small>
           </div>
         ))}
