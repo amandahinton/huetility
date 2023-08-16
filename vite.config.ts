5fb0bd04b7f9e36c0d6926ae09d6f3f186a725cf
Amanda <hello@amandahinton.com>
2023-08-15 23:06:19 -0400

    create aliases for main directories

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: path.resolve(__dirname, "./src/components/"),
+      contexts: path.resolve(__dirname, "./src/contexts/"),
+      types: path.resolve(__dirname, "./src/types/"),
+      utils: path.resolve(__dirname, "./src/utils/"),
+    },
+  },
+});
