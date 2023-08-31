f41b0c566e56084e0aed022d6d9d997ced7f57b4
Amanda <hello@amandahinton.com>
2023-08-31 15:41:40 -0400

    click harmony swatches to copy color to clipboard

--- src/components/PaletteSwatches.tsx
+++ src/components/PaletteSwatches.tsx
@@ -1,21 +1,29 @@
 import { Tooltip } from "components/index";
+import { useColor } from "contexts/ColorContext";
 import { Palette } from "@/types/types";
-import { contrastTextHex } from "utils/helpers";
+import { contrastTextHex, cssColorValue } from "utils/helpers";
 import "components/PaletteSwatches.css";
 
 export function PaletteSwatches({ palette }: { palette: Palette }) {
+  const { color } = useColor();
+  const { colorMode } = color;
+
   return (
     <div className="huetility-palette-container">
       <div className="huetility-swatch-group huetility-bordered">
-        {palette.paletteColors.map((color) => (
-          <div
-            key={color.HEX}
-            className={"huetility-palette-swatch"}
-            style={{ backgroundColor: color.HEX }}
-          >
-            <small style={{ color: contrastTextHex(color) }}>{color.HEX}</small>
-          </div>
-        ))}
+        {palette.paletteColors.map((swatch) => {
+          const cssSwatch = cssColorValue(colorMode, swatch);
+          return (
+            <button
+              key={swatch.HEX}
+              onClick={() => navigator.clipboard.writeText(cssSwatch)}
+              className={"huetility-palette-swatch"}
+              title={`Click to copy: ${cssSwatch}`}
+              aria-label={`Click to copy: ${cssSwatch}`}
+              style={{ backgroundColor: swatch.HEX }}
+            ></button>
+          );
+        })}
       </div>
 
       <Tooltip hoverChildren message={palette.description}>
