d2d40be900a27a4724cb1d9f045a91f8845e1790
Amanda <hello@amandahinton.com>
2023-10-02 18:13:57 -0400

    reorder exports to match directory structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,21 +1,28 @@
-export * from "components/Canvas.tsx";
+export * from "components/contrast/Contrast.tsx";
+export * from "components/contrast/ContrastChecker.tsx";
+
+export * from "components/harmonies/Harmonies.tsx";
+
 export * from "components/picker/ColorPicker.tsx";
 export * from "components/picker/PickerHEX.tsx";
 export * from "components/picker/PickerHSL.tsx";
 export * from "components/picker/PickerHSLA.tsx";
 export * from "components/picker/PickerRGB.tsx";
 export * from "components/picker/PickerRGBA.tsx";
-export * from "components/contrast/Contrast.tsx";
-export * from "components/contrast/ContrastChecker.tsx";
-export * from "components/values/CSSValues.tsx";
-export * from "components//harmonies/Harmonies.tsx";
-export * from "components/InputHex.tsx";
-export * from "components/PaletteSwatches.tsx";
 export * from "components/picker/RGBColorWheel.tsx";
-export * from "components/scales/Scales.tsx"
+
+export * from "components/scales/Scales.tsx";
 export * from "components/scales/Shades.tsx";
 export * from "components/scales/Tints.tsx";
+
+export * from "components/values/CSSValues.tsx";
+
+export * from "components/vision/Vision.tsx";
+
+export * from "components/Canvas.tsx";
+export * from "components/Footer.tsx";
+export * from "components/InputHex.tsx";
+export * from "components/PaletteSwatches.tsx";
 export * from "components/Swatch.tsx";
 export * from "components/Testing.tsx";
 export * from "components/Tooltip.tsx";
-export * from "components/vision/Vision.tsx";
