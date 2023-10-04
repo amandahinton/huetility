83bd1195bf2a70424a51fe1533afdfa4440685b9
Amanda <hello@amandahinton.com>
2023-10-04 16:41:18 -0400

    prettier

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,7 +1,8 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
 import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
+
 // clear jsdom and clean up after each test case
 afterEach(() => {
   cleanup();
