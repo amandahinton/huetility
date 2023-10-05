659e09674cf0edfcc6eb9dc73651be4c461bb470
Amanda <hello@amandahinton.com>
2023-10-04 23:16:39 -0400

    add alias for test directory

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/"),
       components: path.resolve(__dirname, "./src/components/"),
       contexts: path.resolve(__dirname, "./src/contexts/"),
+      tests: path.resolve(__dirname, "./src/tests/"),
       types: path.resolve(__dirname, "./src/types/"),
       utils: path.resolve(__dirname, "./src/utils/"),
     },
